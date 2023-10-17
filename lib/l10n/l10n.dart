f49ddcb96f5330864268e067798d8302de170706
Antoine • Developer <antoine.gonthier@epitech.eu>
2023-10-17 21:30:03 +0200

    🗣️ multi-language translation

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class L10n {
+  static final List<Locale> all = <Locale>[
+    const Locale('en'),
+    const Locale('fr'),
+    const Locale('it'),
+    const Locale('es'),
+    const Locale('de'),
+  ];
+}
